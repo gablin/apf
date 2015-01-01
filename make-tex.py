580897c1faf6f992c67936da2456b54bcfe3e8f9
Gabriel Hjort Blindell <ghb@kth.se>
2015-01-01 02:16:08 +0100

    Added new Thud annotation.

--- make-tex.py
+++ make-tex.py
@@ -551,6 +551,7 @@ def toLatexSub(s):
                   )
                  )
     s = s.replace("360-degree", "360$^{\\circ}$")
+    s = s.replace("Khazad-dum", "Khazad-d\\^um")
 
     # Typeset certain text parts as Death
     s = s.replace("\"I DON'T KNOW ABOUT YOU, BUT I COULD MURDER A CURRY\"",
